f5191aa995d978880485bab58de6116e2bbd7dfc
Menard <perez.menard.nomiddlename@gmail.com>
2024-04-22 20:52:52 +0800

    initial commit

---
+++ Dashboard.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" 
+    href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="global.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-9">
+                <div class="searchInputContainer">
+                    <input type="text" class="searchInput" placeholder="Search Product" >
+                </div>
+                <div class="searchResultContainer">
+                    <div class="row">
+                      
+                    <div class="col-4">
+                        <div class="productResultContainer">
+                         <img src="images/gabe.jpg" width="100%" alt="">
+                        <div class="productInfoContainer">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <p class="productName">
+                                       Toblerone
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p class="productPrice">
+                                        1000
+                                    </p>
+                                </div>
+                            </div>
+                         </div>
+                        </div> 
+                        
+                    </div>
+                       
+                    <div class="col-4">
+                        <div class="productResultContainer">
+                         <img src="images/gabe.jpg" width="100%" alt="">
+                        <div class="productInfoContainer">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <p class="productName">
+                                       Toblerone
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p class="productPrice">
+                                        1000
+                                    </p>
+                                </div>
+                            </div>
+                         </div>
+                        </div> 
+                        
+                    </div>
+                       
+                    <div class="col-4">
+                        <div class="productResultContainer">
+                         <img src="images/gabe.jpg" width="100%" alt="">
+                        <div class="productInfoContainer">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <p class="productName">
+                                       Toblerone
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p class="productPrice">
+                                        1000
+                                    </p>
+                                </div>
+                            </div>
+                         </div>
+                        </div> 
+                        
+                    </div>
+                       
+                    <div class="col-4">
+                        <div class="productResultContainer">
+                         <img src="images/gabe.jpg" width="100%" alt="">
+                        <div class="productInfoContainer">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <p class="productName">
+                                       Toblerone
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p class="productPrice">
+                                        1000
+                                    </p>
+                                </div>
+                            </div>
+                         </div>
+                        </div> 
+                        
+                    </div>
+                       
+                    <div class="col-4">
+                        <div class="productResultContainer">
+                         <img src="images/gabe.jpg" width="100%" alt="">
+                        <div class="productInfoContainer">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <p class="productName">
+                                       Toblerone
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p class="productPrice">
+                                        1000
+                                    </p>
+                                </div>
+                            </div>
+                         </div>
+                        </div> 
+                        
+                    </div>
+                       
+                    <div class="col-4">
+                        <div class="productResultContainer">
+                         <img src="images/gabe.jpg" width="100%" alt="">
+                        <div class="productInfoContainer">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <p class="productName">
+                                       Toblerone
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p class="productPrice">
+                                        1000
+                                    </p>
+                                </div>
+                            </div>
+                         </div>
+                        </div> 
+                        
+                    </div>
+                       
+                    <div class="col-4">
+                        <div class="productResultContainer">
+                         <img src="images/gabe.jpg" width="100%" alt="">
+                        <div class="productInfoContainer">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <p class="productName">
+                                       Toblerone
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p class="productPrice">
+                                        1000
+                                    </p>
+                                </div>
+                            </div>
+                         </div>
+                        </div> 
+                        
+                    </div>
+                       
+                    <div class="col-4">
+                        <div class="productResultContainer">
+                         <img src="images/gabe.jpg" width="100%" alt="">
+                        <div class="productInfoContainer">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <p class="productName">
+                                       Toblerone
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p class="productPrice">
+                                        1000
+                                    </p>
+                                </div>
+                            </div>
+                         </div>
+                        </div> 
+                        
+                    </div>
+                       
+                    <div class="col-4">
+                        <div class="productResultContainer">
+                         <img src="images/gabe.jpg" width="100%" alt="">
+                        <div class="productInfoContainer">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <p class="productName">
+                                       Toblerone
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p class="productPrice">
+                                        1000
+                                    </p>
+                                </div>
+                            </div>
+                         </div>
+                        </div> 
+                        
+                    </div>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="col-3">
+                col2
+            </div>
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" 
+integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" 
+crossorigin="anonymous"></script>
+</html>

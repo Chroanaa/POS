2059bf0d8e7a96e9cbcbb4a24dede29c40623dff
Menard <perez.menard.nomiddlename@gmail.com>
2024-04-23 20:08:49 +0800

    done with POS page

--- POS page.php
+++ POS page.php
@@ -3,17 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>POS</title>
     <link rel="stylesheet" 
     href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
     crossorigin="anonymous">
-    <link rel="stylesheet" href="global.css">
+    <link rel="stylesheet" href="global.css?v=<?= time()?>" type="text/css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 </head>
 <body>
-    <div class="container">
+    <div class="container-fluid">
+      <!---- Products column-row ---->
         <div class="row">
-            <div class="col-9">
+            <div class="col-8" >
                 <div class="searchInputContainer">
                     <input type="text" class="searchInput" placeholder="Search Product" >
                 </div>
@@ -211,11 +214,89 @@
                     </div>
                     
                 </div>
+                      <!---- Products column-row ---->
+
             </div>
-            <div class="col-3">
-                col2
+            <div class="col-4 posOrderContainer">
+                <div class="pos_header" >
+                  <div class="setting alignRight" style="text-align: right !important;">
+                    <a href="javascript:void(0);"><i class="fa fa-gear"></i></a>
+                  </div>
+                  <p class="logo">IMS</p>
+                  <p class="timeAndDate">April 23, 2024 11:34AM</p>
+                </div>
+                <div class="pos_items_container">
+                    <div class="pos_items">
+                     <table class = "table" id="pos_items_tbl">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>PRODUCT</th>
+                                <th>PRICE</th>
+                                <th>AMOUNT</th>
+                                <th>TOTAL</th>
+                            </tr>
+                        </thead>
+                        <tbody >
+                            <tr>
+                                <td>1</td>
+                                <td>Milo</td>
+                                <td>$32</td>
+                                <td>5</td>
+                                <td>$160</td>
+                                <td>
+                                    <a href="javascript:void(0)" class="pos_items_btn"><i class="fa fa-edit"></i></a>
+                                     <a href="javascript:void(0)" class="pos_items_btn"><i class="fa fa-trash"></i></a>
+                                </td>
+                            </tr>
+                             <tr>
+                                <td>2</td>
+                                <td>Milo</td>
+                                <td>$32</td>
+                                <td>5</td>
+                                <td>$160</td>
+                                <td>
+                                    <a href="javascript:void(0)" class="pos_items_btn"><i class="fa fa-edit"></i></a>
+                                     <a href="javascript:void(0)" class="pos_items_btn"><i class="fa fa-trash"></i></a>
+                                </td>
+                            </tr>
+                             <tr>
+                                <td>3</td>
+                                <td>Milo</td>
+                                <td>$32</td>
+                                <td>5</td>
+                                <td>$160</td>
+                                <td>
+                                    <a href="javascript:void(0)" class="pos_items_btn"><i class="fa fa-edit"></i></a>
+                                     <a href="javascript:void(0)" class="pos_items_btn"><i class="fa fa-trash"></i></a>
+                                </td>
+                            </tr>
+                             <tr>
+                                <td>4</td>
+                                <td>Milo</td>
+                                <td>$32</td>
+                                <td>5</td>
+                                <td>$160</td>
+                                <td>
+                                    <a href="javascript:void(0)" class="pos_items_btn"><i class="fa fa-edit"></i></a>
+                                     <a href="javascript:void(0)" class="pos_items_btn"><i class="fa fa-trash"></i></a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </div>
+                  <div class="item_total_container">
+                    <p class="item_total">
+                        <span class="item_total--label">TOTAL</span>
+                        <span class="item_total--value">$500</span>
+                    </p>
+                </div>
+                <div class = "checkoutBtnContainer">
+                    <a href="javascript:void(0)" class="checkoutBtn">CHECKOUT</a>
+                </div>
             </div>
         </div>
+        
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" 
